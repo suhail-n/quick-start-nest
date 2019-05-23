c7230f9b2dba464f96cee926318ab5f6f1bf0d53
Suhail Nooristani <suhailnooristani@Suhails-MacBook-Air.local>
2019-05-23 00:56:35 -0400

    Removed cats controller and service. Added cats module in the imports instead

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
+  imports: [CatsModule],
+  controllers: [AppController],
+  // services, repositories, factories, helpers, etc.. Allows to inject them
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
