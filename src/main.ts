465eb8875ecfd1f99ed38d10791da6c5df47e913
Suhail Nooristani <suhailnooristani@Suhails-MacBook-Air.local>
2019-05-26 12:04:02 -0400

    added comments

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { functionalLogger } from './common/middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // use logger middle ware on every registered route at once.
+  // app.use(functionalLogger);
   await app.listen(3000);
 }
 bootstrap();
