1581272566f6e81210a8a179c5ce4a02d647b645
Suhail Nooristani <suhailnooristani@Suhails-MacBook-Air.local>
2019-05-26 12:07:34 -0400

    commented out old configure from nestmodule

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
+// with global, modules will no longer need to import CatsModule in their imports array
+// @Global()
 @Module({
     controllers: [CatsController],
     providers: [CatsService]
 })
 // this needs to be imported in the root module under imports or just directly import cats controllers and services which is bad practice
-export class CatsModule { }
+export class CatsModule {
+
+    // configure(consumer: MiddlewareConsumer): void | MiddlewareConsumer {
+    //     // applying multiple middlewares
+    //     consumer
+    //         .apply(LoggerMiddleware, functionalLogger)
+    //         .exclude({ path: 'cats', method: RequestMethod.PUT })
+    //         .forRoutes(CatsController);
+    // }
+}
