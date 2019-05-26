e847fcd541063038debabccd541623619092322d
Suhail Nooristani <suhailnooristani@Suhails-MacBook-Air.local>
2019-05-26 12:37:32 -0400

    Created custom exception filter

---
+++ src/common/exception/http-exception.filter.ts
@@ -0,0 +1,20 @@
+import { HttpException, Catch, ExceptionFilter, ArgumentsHost } from "@nestjs/common";
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+    // ArgumentsHost is a wrapper for arguments passed to the original request handler
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response: Response = ctx.getResponse<Response>();
+        const request: Request = ctx.getRequest<Request>();
+        const status = exception.getStatus();
+        response
+            .status(status)
+            .json({
+                statusCode: status,
+                timestamp: new Date().toISOString(),
+                path: request.url
+            });
+    }
+}
