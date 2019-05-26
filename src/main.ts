31f3fb52c1be389e5dbe250ddb5e5e63f459406e
Suhail Nooristani <suhailnooristani@Suhails-MacBook-Air.local>
2019-05-26 12:53:14 -0400

    Commented out global exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { HttpExceptionFilter } from './common/exception/http-exception.filter';
 // import { functionalLogger } from './common/middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // use logger middle ware on every registered route at once.
   // app.use(functionalLogger);
+
+  // Exception filters can also be used globally like this.
+  // app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
