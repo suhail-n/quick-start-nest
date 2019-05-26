cb1c4a395a09e1d8dc1b0d7e3570a028bd9374d4
Suhail Nooristani <suhailnooristani@Suhails-MacBook-Air.local>
2019-05-26 11:44:05 -0400

    Implemented nestmodule to add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware, functionalLogger } from './common/middleware/logger.middleware';
+import { CatsController } from './cats/cats.controller';
 
+// app module used for registering providers
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
   // services, repositories, factories, helpers, etc.. Allows to inject them
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void | MiddlewareConsumer {
+    // applying multiple middlewares
+    consumer
+      // middlewares get called in this order of apply
+      .apply(LoggerMiddleware, functionalLogger)
+      .exclude(
+        // exlude these paths from the middleware
+        { path: '/cats', method: RequestMethod.ALL },
+        { path: '/cats/:id', method: RequestMethod.PUT })
+        // target CatsController for middleware
+      .forRoutes(CatsController);
+  }
+}
